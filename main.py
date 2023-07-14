e23b74321a213727505006da91baeff9de4eaa40
Hameedhul Arshadh A <Hameedhul.Arshadh.A@ibm.com>
2023-07-15 00:59:50 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+# imports
+from gtts import gTTS
+import PyPDF2
+
+
+def readTextFile(filename):
+    with open(filename, "r") as file:
+        return file.read()
+
+def readPDFFile(filename):
+    text = ""
+    pdfreader = PyPDF2.PdfReader(filename)
+    for page in range(len(pdfreader.pages)-40):
+        content = pdfreader.pages[page]
+        text = text + content.extract_text()
+    return text
+
+if __name__ == '__main__':
+    filename = "TwoFriendsAndTheBear.txt"
+    tld = 'co.in'
+    text = readTextFile(filename)
+    audioBook = gTTS(text, tld=tld, slow=False)
+    audioBook.save(str(filename.split(".")[0]) + ".mp3")
+
+    filename = "10 Vertebrates F.pdf"
+    text = readPDFFile(filename)
+    # audioBook = gTTS(text, tld=tld, slow=False)
+    # audioBook.save(str(filename.split(".")[0]) + ".mp3")
